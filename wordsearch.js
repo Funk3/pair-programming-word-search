707d803dba9b189005aa2875867353ac254f5a26
Zak <zak.johansson@gmail.com>
2022-11-23 14:59:47 -0800

    used tranpose function to make vertical lines horizontal, and recheck the horizontal lines to see if word exists

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
+const transpose = function (matrix) {
+    let array = [];
+    for(let i = 0; i < matrix[0].length; i++){
+      array.push([])
+    }
+    for (let row = 0; row < matrix.length; row++){
+      for (let collumn = 0; collumn < matrix[row].length; collumn++){
+        array[collumn][row] = matrix[row][collumn] 
+      }
+    }
+    return array;
+  };
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = transpose(letters)
+    const horizontalJoin2 = verticalJoin.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+        if (l.includes(word)) {
+            return true
+            }
+            for (l of horizontalJoin2) {
+                for (l of horizontalJoin) {
+                    if (l.includes(word)) return true
+                } 
+                return false
+            }   
+        }
+
+   
 }
 
 module.exports = wordSearch
